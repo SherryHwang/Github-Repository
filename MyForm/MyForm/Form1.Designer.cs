11c2718414410287c930ffad2d09909e629d95c0
Sherry Hwang <1354789084@qq.com>
2018-07-20 19:36:56 +0800

    MyForm
    MyForm Project...

---
+++ MyForm/MyForm/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace MyForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(352, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 264);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(21, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(352, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(104, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(387, 274);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Location = new System.Drawing.Point(104, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(387, 274);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabControl1.ItemSize = new System.Drawing.Size(30, 100);
+            this.tabControl1.Location = new System.Drawing.Point(561, 63);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(495, 282);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl1.TabIndex = 4;
+            this.tabControl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabControl1_DrawItem);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 112);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 43);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "this is page2";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(118, 116);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 43);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.Text = "this is page1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 385);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Serail_Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ MyForm/MyForm/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace MyForm
+{
+   
+    public partial class Form1 : Form
+    {
+        private SerialPort ComDevice = new SerialPort();
+        private int i = 1;
+        private string str = "";
+        
+        public Form1()
+        {
+            Image image1 = Image.FromFile(@"image.jpg");
+            this.BackgroundImage = image1;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+ 
+
+
+
+
+            InitializeComponent();
+            ComDevice.PortName = "COM3";
+            ComDevice.BaudRate = 9600;
+        }
+
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string str = textBox2.Text;
+            ComDevice.Open();
+            ComDevice.Write(str);  //发送数据
+            Thread.Sleep(100);
+            //MessageBox.Show("发送的数据：" + str);
+            int n = ComDevice.BytesToRead; //获取字节数
+            byte[] buf = new byte[n];//声明一个临时数组存储当前来的串口数据 
+            Thread.Sleep(100);
+            ComDevice.Read(buf, 0, n);
+            this.Invoke((EventHandler)(delegate
+            {
+                //直接按ASCII规则转换成字符串   
+                str = "第";
+                str += i++;
+                str += "次接收到的数据为：";
+                str += Encoding.ASCII.GetString(buf);
+                str += "\r\n";
+                textBox1.AppendText(str);
+                //MessageBox.Show("接收到数据长度：" +buf.Length);
+            }));
+            ComDevice.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image image2 = Image.FromFile(@"button_logo1.jpg");
+            this.button1.BackgroundImage = image2;
+            this.button1.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            Image image2 = Image.FromFile(@"button_logo1.jpg");
+            this.button1.BackgroundImage = image2;
+            this.button1.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Image image2 = Image.FromFile(@"button_logo2.jpg");
+            this.button1.BackgroundImage = image2;
+            this.button1.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void tabControl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            
+            string text = ((TabControl)sender).TabPages[e.Index].Text;
+
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            StringFormat sf = new StringFormat(StringFormatFlags.DirectionRightToLeft);
+
+            sf.LineAlignment = StringAlignment.Center;
+
+            sf.Alignment = StringAlignment.Center;
+
+            e.Graphics.DrawString(text, SystemInformation.MenuFont, brush, e.Bounds, sf);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
